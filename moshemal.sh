f3bd8748f579800e9b1f483418a0283733573b89
Grant Rettke <grant@wisdomandwonder.com>
2021-03-31 12:01:50 -0500

    Sock url.

--- moshemal.sh
+++ moshemal.sh
@@ -3,7 +3,7 @@
 
 function moshemal() {
   local code="$1"
-  local val=$(/usr/local/bin/emacs --batch --eval "(progn (require 'server) (princ (server-eval-at \"/Users/gcr/server-sockets/emacs-gcr.sock\" '$code)))")
+  local val=$(/usr/local/bin/emacs --batch --eval "(progn (require 'server) (princ (server-eval-at \"/Users/gcr/server-sockets/emacs.sock\" '$code)))")
   printf "%s\n" "$val"
 }
 
