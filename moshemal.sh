47fbdf635c59eb199736bb55cc2a2d1255b40a4d
Grant Rettke <grant@wisdomandwonder.com>
2021-03-31 13:02:43 -0500

    Check args.

--- moshemal.sh
+++ moshemal.sh
@@ -2,9 +2,12 @@
 # shellcheck disable=SC2039,SC2112
 
 function moshemal() {
+  if [ "$#" -ne 1 ] || [ -z "$1" ] ; then
+    printf "Evaluate a single-line EXPR on a local Emacs instance defined in the variable EMACSSOCKET.\n"
+    printf "Usage: %s EXPR.\n" "${FUNCNAME[0]} "
+    return 1
+  fi
   local code="$1"
   local val=$(/usr/local/bin/emacs --batch --eval "(progn (require 'server) (princ (server-eval-at \"$EMACSSOCKET\" '$code)))")
   printf "%s\n" "$val"
 }
-
-moshemal "$1"
