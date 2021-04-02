11a1f7f2e96b96f8e5e3e9d0aa008292b2c08164
Grant Rettke <grant@wisdomandwonder.com>
2021-04-01 20:55:31 -0500

    Format

---
+++ moshemal.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+
+export MOSHEMALSOCKET="/Users/gcr/server-sockets/emacs.sock"
+export MOSHEMALHOME="/Users/gcr/src/moshemal"
+
+source "$MOSHEMALHOME/moshemallib.sh"
+
+moshemal "$1"
