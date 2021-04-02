d2807d16310436937280f5bb05c933657a8e04eb
Grant Rettke <grant@wisdomandwonder.com>
2021-04-01 20:02:28 -0500

    Typo

---
+++ moshemallib.sh
@@ -0,0 +1,13 @@
+# shellcheck disable=SC2039,SC2112
+
+function moshemal() {
+  if [ "$#" -ne 1 ] || [ -z "$1" ] ; then
+    printf "Evaluate a single-line EXPR on a local Emacs instance defined in the variable MOSHEMALSOCKET.\n"
+    printf "Usage: %s EXPR.\n" "${FUNCNAME[0]} "
+    return 1
+  fi
+  local code="$1"
+  local val
+  val=$(/usr/local/bin/emacs --batch --eval "(progn (require 'server) (princ (server-eval-at \"$MOSHEMALSOCKET\" '$code)))")
+  printf "%s\n" "$val"
+}
